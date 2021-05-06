d4a57c9b28bdd6761848146ed0ad5cdb88657303
陈小瓜 <7780549+chen_xiaogua@user.noreply.gitee.com>
2021-05-06 15:56:54 +0800

    e

--- pages/components/Navitem.js
+++ pages/components/Navitem.js
@@ -4,12 +4,13 @@ import styled from '@emotion/styled'
 
 
 export default function Navitem({data}) {
-    // console.log(data.url)
+    const url = data.url | 'https://edu.lagou.com/'
+    const name = data.name
     return (
         <Box m="10px"  w="108px" h="108px">
-            <Box as="a" href={data.url}  w="108px" h="108px" target="_blank" display="block" border="1px solid #ddd" >
-                <Image mb="10px" w="100%"  alt={data.name} />
-                <Text textAlign="center" fontSize="xl">{data.name}</Text>
+            <Box as="a" href={url}  w="108px" h="108px" target="_blank" display="block" border="1px solid #ddd" >
+                <Image mb="10px" w="100%"  alt={name} />
+                <Text textAlign="center" fontSize="xl">{name}</Text>
             </Box>
         </Box>
     )
